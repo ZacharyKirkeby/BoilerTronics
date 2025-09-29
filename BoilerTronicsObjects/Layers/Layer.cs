606a6ee8747edd76264df0074958afa9f012982b
Koeh1124 <keenankoehler@gmail.com>
2025-09-28 21:42:42 -0400

    created more scaffolding and implemented some functions for the layers

--- BoilerTronicsObjects/Layers/Layer.cs
+++ BoilerTronicsObjects/Layers/Layer.cs
@@ -7,12 +7,34 @@ namespace BoilerTronicsObjects.Layers
 {
     public partial class Layer : Godot.TileMapLayer
     {
-        ArrayList objectList = new ArrayList();
+        ArrayList objectList = new ArrayList();     // List of objects that exist on the layer
+        int numItems = 0;                           // Number of items in this layer
 
         public void addObject(PlacableObject newPlacabel)
         {
+            if (newPlacabel == null) return;
             objectList.Add(newPlacabel); // adds the placabel to the list of objects on this layer
+            numItems++;
         }
 
+        public void removeObject(PlacableObject objectToRemove)
+        {
+            if (!objectList.Contains(objectToRemove)) return;
+            objectList.Remove(objectToRemove); // remove to object form the list
+            numItems--;
+        }
+
+        public PlacableObject findObject(Vector2 loc)
+        {
+            for (int objIndex = 0; objIndex < numItems; objIndex++)
+            {
+                PlacableObject obj = (PlacableObject)objectList[objIndex];
+                if (obj == null) continue; // The item in the list was not a placable object    
+                Vector2 pos = obj.getPos();
+                if (pos.X == loc.X && pos.Y == loc.Y) return obj; // We found the object!!
+            }
+            
+            return null; // object was not found
+        }
     }
 }
