a24fb2405f180f0dfc6d078d49e2a77170ad9979
Koeh1124 <keenankoehler@gmail.com>
2025-10-01 20:45:41 -0400

    added more scaffolding for the level state and the global manager

--- BoilerTronicsObjects/GameState/BoilerTronicsGlobalManager.cs
+++ BoilerTronicsObjects/GameState/BoilerTronicsGlobalManager.cs
@@ -4,3 +4,21 @@
 //      - Pass save data into a level (using the same mechanism as mentioned above)
 //      - Aquire the user's scores for the UI
 //      - Keep track of the user's progression
+
+using Godot;
+
+public partial class BoilerTronicsGlobalManager : Node
+{
+    public static BoilerTronicsGlobalManager GlobalManager { get; private set; } // This will be the global singelton we interact with throught the program
+
+    public override void _Ready()
+    {
+        // Make sure there only exists on manager
+        if (GlobalManager != null)
+        {
+            // TODO: make error here   
+        }
+
+        GlobalManager = this; // get this as the manager
+    }
+}
