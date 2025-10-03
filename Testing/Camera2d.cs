087ab14a9b6119b1e3cf0c7d7c504e41e9e1dedb
ethensnchen <143560350+ethensnchen@users.noreply.github.com>
2025-10-03 18:35:55 -0400

    basic camera functionality
    - added very basic Camera2D to interact with more to pan/zoom and etc

---
+++ Testing/Camera2d.cs
@@ -0,0 +1,24 @@
+using Godot;
+using System;
+
+public partial class Camera2d : Camera2D
+{
+	Vector2 OldMousePosition;
+	Vector2 NewMousePosition;
+	
+	public override void _Input(InputEvent @event)
+		{
+			// Note: these inputs will still "pass through" and Layer.cs could theoretically receive this
+			// Be advised!
+			// TODO: design a system for panning the view screen using right mouse button
+			if (@event is InputEventMouseButton buttonEvent && buttonEvent.ButtonIndex == MouseButton.Right && buttonEvent.Pressed)
+			{
+				NewMousePosition = GetViewport().GetMousePosition();
+				
+				this.Position += new Vector2(5, 5);
+				
+				OldMousePosition = GetViewport().GetMousePosition();
+			}
+			// base._Input(@event); // Calling this will pass down the input, we want to absorbe it
+		}
+}
