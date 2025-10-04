b5fd8c89222372ea153f7b45ac142a8ab6132365
Abhi <doshi47@purdue.edu>
2025-10-03 21:21:44 -0400

    Added functionality for step/reset buttons for step counter

--- Scenes/LevelUi.cs
+++ Scenes/LevelUi.cs
@@ -3,7 +3,33 @@ using System;
 
 public partial class LevelUi : Node2D
 {
+
+	private Label stepCountLabel;
+	private int stepCount = 0;
+	bool error = true;
+
+	public override void _Ready() {
+		stepCountLabel = GetNode<Label>("%Step Count");
+		UpdateStepCount();
+	}
+	
 	private void _on_button_pressed() {
 		GetTree().ChangeSceneToFile("res://Scenes/main_menu.tscn");
+	}
+	
+	private void _on_step_button_pressed() {
+		if(!error) {
+			stepCount++;
+			UpdateStepCount();
+		}
+	}
+
+	private void UpdateStepCount() {
+		stepCountLabel.Text = "Step Count: " + stepCount;
+	}
+	
+	private void _on_reset_button_pressed() {
+		stepCount = 0;
+		UpdateStepCount();
 	}
 }
