3459ab8ff2cc5dc6220904f6d967d9758d13670f
Koeh1124 <keenankoehler@gmail.com>
2025-10-04 15:35:09 -0400

    added scripts to the selection buttons for the items in the test level UI, ad made them change a value in the global manager

--- Resources/Scripts/Testing/LevelUI/PlaceButton1.cs
+++ Resources/Scripts/Testing/LevelUI/PlaceButton1.cs
@@ -3,5 +3,18 @@ using System;
 
 public partial class PlaceButton1 : Button
 {
-    
+	public override void _Ready()
+	{
+		Button PlaceButton1 = this;
+		PlaceButton1.Pressed += _press_func;
+	}
+
+	private void _press_func()
+	{
+		// DO STUFF HERE
+		BoilerTronicsGlobalManager manager = BoilerTronicsGlobalManager.GlobalManager;
+		GD.Print("old selection ", manager.currSlection);
+		manager.currSlection = 1;
+		GD.Print("new selection ", manager.currSlection);
+	}
 }
--- Resources/Scripts/Testing/LevelUI/PlaceButton3.cs
+++ Resources/Scripts/Testing/LevelUI/PlaceButton3.cs
@@ -3,5 +3,19 @@ using System;
 
 public partial class PlaceButton3 : Button
 {
+	public override void _Ready()
+	{
+		Button PlaceButton3 = this;
+		PlaceButton3.Pressed += _press_func;
+	}
 
+	private void _press_func()
+	{
+		// DO STUFF HERE
+		// GD.Print("test3");
+		BoilerTronicsGlobalManager manager = BoilerTronicsGlobalManager.GlobalManager;
+		GD.Print("old selection ", manager.currSlection);
+		manager.currSlection = 3;
+		GD.Print("new selection ", manager.currSlection);
+	}
 }
