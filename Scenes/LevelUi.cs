2eb63b240ca286b2a6df43d248619aaff922b9ac
Abhi <doshi47@purdue.edu>
2025-10-04 20:34:35 -0400

    auto removal of error notice on reset and basic text color altering

--- Scenes/LevelUi.cs
+++ Scenes/LevelUi.cs
@@ -6,10 +6,11 @@ public partial class LevelUi : Node2D
 
 	private Label stepCountLabel;
 	private int stepCount = 0;
-	bool error = false;
+	bool error = false; //temp boolean to track if an error has occured
+	private Node2D errorNoticeIcon;
 
 	public override void _Ready() {
-		stepCountLabel = GetNode<Label>("%Step Count");
+		stepCountLabel = GetNode<Label>("%Step Count"); //unique identifier for the step counter
 		UpdateStepCount();
 	}
 	
@@ -21,6 +22,24 @@ public partial class LevelUi : Node2D
 		if(!error) {
 			stepCount++;
 			UpdateStepCount();
+			
+			//TODO: dynamic text coloring
+			stepCountLabel.AddThemeColorOverride("font_color", new Color(1.0f, 1.0f, 1.0f, 1.0f));
+		}
+		else {
+			//open error notice (exclamation mark) at coords of error
+			if(errorNoticeIcon == null) {
+				var scene = (PackedScene)ResourceLoader.Load("res://Resources/ErrorNotice.tscn");
+				errorNoticeIcon = scene.Instantiate<Node2D>();
+				AddChild(errorNoticeIcon);
+				//TODO: replace example coords with actual (make dynamic)
+				errorNoticeIcon.Position = new Vector2(1000, 600);
+			}
+
+			//TODO: replace example with actual error (make dynamic)
+			var packedErrorScene = ResourceLoader.Load<PackedScene>("Resources/ClawRailError.tscn");
+			var instance = packedErrorScene.Instantiate();
+			GetTree().CurrentScene.AddChild(instance);
 		}
 	}
 
@@ -30,6 +49,12 @@ public partial class LevelUi : Node2D
 	
 	private void _on_reset_button_pressed() {
 		stepCount = 0;
+		stepCountLabel.AddThemeColorOverride("font_color", new Color(0.67f, 0.67f, 0.67f, 0.86f));
 		UpdateStepCount();
+		//delete error notice (exclamation mark) if exists/open
+		if(errorNoticeIcon != null) {
+			errorNoticeIcon.QueueFree();
+			errorNoticeIcon = null;
+		}
 	}
 }
