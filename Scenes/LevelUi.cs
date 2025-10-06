7be61e3de48e9295998a616a0084c6855faa7efb
dixon140 <dixon140@purdue.edu>
2025-10-06 12:18:07 -0400

    added time control icons and ability to switch between item tabs

--- Scenes/LevelUi.cs
+++ Scenes/LevelUi.cs
@@ -3,7 +3,29 @@ using System;
 
 public partial class LevelUi : Node2D
 {
+	public override void _Ready() {
+		var button = GetNode<Button>("MainVBox/PanelContainer/HBoxContainer/CategoryPicker/Movement");
+		button.GrabFocus();
+	}
 	private void _on_button_pressed() {
 		GetTree().ChangeSceneToFile("res://Scenes/main_menu.tscn");
+	}
+	private void _on_movement_pressed() {
+		GetNode<Control>("MainVBox/PanelContainer/HBoxContainer/PanelContainer/Claw Items").Visible = false;
+		GetNode<Control>("MainVBox/PanelContainer/HBoxContainer/PanelContainer/Factory Items").Visible = false;
+		GetNode<ScrollContainer>("MainVBox/PanelContainer/HBoxContainer/PanelContainer/Movement Items").ScrollHorizontal = 0;
+		GetNode<Control>("MainVBox/PanelContainer/HBoxContainer/PanelContainer/Movement Items").Visible = true;
+	}
+	private void _on_claw_pressed() {
+		GetNode<Control>("MainVBox/PanelContainer/HBoxContainer/PanelContainer/Factory Items").Visible = false;
+		GetNode<Control>("MainVBox/PanelContainer/HBoxContainer/PanelContainer/Movement Items").Visible = false;
+		GetNode<ScrollContainer>("MainVBox/PanelContainer/HBoxContainer/PanelContainer/Claw Items").ScrollHorizontal = 0;
+		GetNode<Control>("MainVBox/PanelContainer/HBoxContainer/PanelContainer/Claw Items").Visible = true;
+	}
+	private void _on_factory_pressed() {
+		GetNode<Control>("MainVBox/PanelContainer/HBoxContainer/PanelContainer/Claw Items").Visible = false;
+		GetNode<Control>("MainVBox/PanelContainer/HBoxContainer/PanelContainer/Movement Items").Visible = false;
+		GetNode<ScrollContainer>("MainVBox/PanelContainer/HBoxContainer/PanelContainer/Factory Items").ScrollHorizontal = 0;
+		GetNode<Control>("MainVBox/PanelContainer/HBoxContainer/PanelContainer/Factory Items").Visible = true;
 	}
 }
