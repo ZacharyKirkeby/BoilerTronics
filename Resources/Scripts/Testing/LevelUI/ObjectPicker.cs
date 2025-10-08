9218771d0aa6b08d760cdd0c68ae788bbc469daf
keenan <keenankoehler@gmail.com>
2025-10-07 20:53:58 -0400

    made it so we can dynamically change the sprites in the test level ui

---
+++ Resources/Scripts/Testing/LevelUI/ObjectPicker.cs
@@ -0,0 +1,109 @@
+using Godot;
+using System;
+
+public partial class ObjectPicker : HBoxContainer
+{
+	// sprite table id for each table
+	static int FactorySpriteTable = 0;
+	static int ClawSpriteTable = 1;
+	static int MovementSpriteTable = 2;
+
+	void Update() 
+	{
+		// This should be called when we change the type of object that we are wanting to select
+		// The currSelect in the manager should be set beforehand as it will use that value to change teh sprites it contains
+		BoilerTronicsGlobalManager manager = BoilerTronicsGlobalManager.GlobalManager;
+		switch (manager.currSlection) {
+			case 1: // Movement
+				SetMovement();
+				break;
+			case 2: // Factory
+				SetFactory();
+				break;
+			case 3: // Claw
+				SetClaw();
+				break;
+			default:
+				break;
+		}
+	}
+
+	void KillChildren()
+	{
+		// This will kill all the children that are sprites
+		// This also need to be modular because the children may need to be different
+		foreach (Node child in GetChildren()) {
+			if (child is Control) { // replace this with different class if we need to wrap the child class in something
+				child.QueueFree(); // This kills the child
+				RemoveChild(child);
+				// GD.Print("Child has been killed");
+			} else {
+				// GD.Print("This is not the right child");
+			}
+		}
+	}
+
+	void loadSprites(int source_idx, int posX, int posY) {
+		var tileSet = GD.Load<TileSet>("res://Resources/objects.tres");
+		int sourceid = tileSet.GetSourceId(source_idx);
+
+		TileSetAtlasSource tileSetSource = tileSet.GetSource(sourceid) as TileSetAtlasSource;
+		if (tileSetSource == null) {
+			GD.Print(source_idx, " is not a valid id for the sprite tabel");
+			return; // make sure it exists
+		}
+		for (int i = 0; i < tileSetSource.GetTilesCount(); i++) {
+			var atlasCords = tileSetSource.GetTileId(i);
+			if (atlasCords == null) continue; // make sure that the cords exist
+
+			// get the tile
+			var tile = tileSetSource.GetTileTextureRegion(atlasCords);
+			var fullTexture = tileSetSource.Texture.GetImage();
+			var imageTexture = fullTexture.GetRegion(tile);
+			var texture = new ImageTexture();
+			texture.SetImage(imageTexture);
+
+			Control objectController = new Control();
+			objectController.SetSize(new Vector2I(128, 128));
+			objectController.Set(Control.PropertyName.CustomMinimumSize, new Vector2I(128, 128));
+			AddChild(objectController);
+			Sprite2D sprite = new Sprite2D();
+			// get texture
+			sprite.Texture = texture;
+			sprite.Scale = new Vector2I(5, 5);
+			sprite.Set(Sprite2D.PropertyName.Position, new Vector2I(posX, posY));
+			objectController.AddChild(sprite);
+			GD.Print(GetChildren());
+		}
+	}
+
+	void SetClaw()
+	{
+		// Kill the current children to ensure that only children that we want exist
+		KillChildren();
+		// This will spawn the children for all of the different kinds of claw layer elements
+		loadSprites(ClawSpriteTable, 128, 196);		
+	}
+
+	void SetFactory()
+	{
+		// Kill the current children to ensure that only children that we want exist
+		KillChildren();
+		// This will spawn the children for all of the different kinds of factory layer elements
+		loadSprites(FactorySpriteTable, 128, 128);		
+	}
+
+	void SetMovement()
+	{
+		// Kill the current children to ensure that only children that we want exist
+		KillChildren();
+		// This will spawn the children for all of the different kinds of movement layer elements
+		loadSprites(MovementSpriteTable, 128, 128);		
+	}
+
+	public override void _Ready()
+	{
+		// This will allow us to have the default selection set here
+		SetMovement();
+	}
+}
