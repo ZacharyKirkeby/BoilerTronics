9007cdd5a9852d3bc23bd3fa1acb63142c048845
Abhi <doshi47@purdue.edu>
2025-10-08 20:33:21 -0400

    made adjustments to functions to improve readability and usability

--- Scenes/LevelUi.cs
+++ Scenes/LevelUi.cs
@@ -6,7 +6,7 @@ public partial class LevelUi : Node2D
 
 	private Label stepCountLabel;
 	private int stepCount = 0;
-	private bool error = true; //temp boolean to track if an error has occured
+	private bool isError = true; //temp boolean to track if an error has occured
 	private Node2D errorNoticeIcon;
 	private String[] errorTypes = {"ClawRail","ClawOutOfBounds","ClawCollision","ClawInventory"}; //keep track of current error type
 	private int currError = 2; //current error type identifier (defined by errorTypes array)
@@ -17,9 +17,10 @@ public partial class LevelUi : Node2D
 		UpdateStepCount();
 	}
 	
-	public void setCurrError(int err) {
+	public void setError(int err) {
 		if((err >= -1) && (err < 4))
 		currError = err;
+		isError = true;
 	}
 	
 	public void setErrorCoords(int x, int y) {
@@ -30,18 +31,57 @@ public partial class LevelUi : Node2D
 		GetTree().ChangeSceneToFile("res://Scenes/main_menu.tscn");
 	}
 
-	private void setError(bool err) {
-		error = err;
+	private void removeError() {
+		isError = false;
+		currError = -1;
 	}
 
-	private void removeError() {
-		error = false;
-		currError = -1;
+	//displays error (specific error popup, location of error on level ui, code terminals highlighted red)
+	private void handleError(int errorType) {
+		//open error notice (exclamation mark) at coords of error
+		if(errorNoticeIcon == null) {
+			var scene = (PackedScene)ResourceLoader.Load("res://Resources/ErrorNotice.tscn");
+			errorNoticeIcon = scene.Instantiate<Node2D>();
+			AddChild(errorNoticeIcon);
+		}
+		//TODO: replace example coords with actual (make dynamic)
+		errorNoticeIcon.Position = errorCoords;
+
+		PackedScene packedErrorScene = null;
+			
+		//highlight all current lines with error coloring
+		var codeEditors = GetTree().GetNodesInGroup("CodeTerminals");
+		foreach (CodeEdit editor in codeEditors)
+		{
+			editor.HighlightLine(editor.getLastHighlighted() + 1, new Color(1, 0, 0, 0.3f));
+		}
+
+		//dynamic error handling
+		switch(currError) {
+			case 0:
+				packedErrorScene = ResourceLoader.Load<PackedScene>("res://Resources/ClawRailError.tscn");
+				break;
+			case 1:
+				packedErrorScene = ResourceLoader.Load<PackedScene>("res://Resources/ClawOutOfBoundsError.tscn");
+				break;
+			case 2:
+				packedErrorScene = ResourceLoader.Load<PackedScene>("res://Resources/ClawCollisionError.tscn");
+				break;
+			case 3:
+				packedErrorScene = ResourceLoader.Load<PackedScene>("res://Resources/ClawInventoryError.tscn");
+				break;
+			case -1:
+				break; //should not happen as error should be set to false
+		}
+		if(packedErrorScene != null) {
+			var instance = packedErrorScene.Instantiate();
+			GetTree().CurrentScene.AddChild(instance);
+		}
 	}
 	
 	private void _on_step_button_pressed() {
 		stepCount++;
-		if(!error) {
+		if(!isError) {
 			UpdateStepCount();
 			
 			stepCountLabel.AddThemeColorOverride("font_color", new Color(1.0f, 1.0f, 1.0f, 1.0f));
@@ -54,44 +94,7 @@ public partial class LevelUi : Node2D
 			}
 		}
 		else {
-			//open error notice (exclamation mark) at coords of error
-			if(errorNoticeIcon == null) {
-				var scene = (PackedScene)ResourceLoader.Load("res://Resources/ErrorNotice.tscn");
-				errorNoticeIcon = scene.Instantiate<Node2D>();
-				AddChild(errorNoticeIcon);
-				//TODO: replace example coords with actual (make dynamic)
-				errorNoticeIcon.Position = errorCoords;
-			}
-			PackedScene packedErrorScene = null;
-			
-			//highlight all current lines with error coloring
-			var codeEditors = GetTree().GetNodesInGroup("CodeTerminals");
-			foreach (CodeEdit editor in codeEditors)
-			{
-				editor.HighlightLine(editor.getLastHighlighted() + 1, new Color(1, 0, 0, 0.3f));
-			}
-
-			//dynamic error handling
-			switch(currError) {
-				case 0:
-					packedErrorScene = ResourceLoader.Load<PackedScene>("res://Resources/ClawRailError.tscn");
-					break;
-				case 1:
-					packedErrorScene = ResourceLoader.Load<PackedScene>("res://Resources/ClawOutOfBoundsError.tscn");
-					break;
-				case 2:
-					packedErrorScene = ResourceLoader.Load<PackedScene>("res://Resources/ClawCollisionError.tscn");
-					break;
-				case 3:
-					packedErrorScene = ResourceLoader.Load<PackedScene>("res://Resources/ClawInventoryError.tscn");
-					break;
-				case -1:
-					break; //should not happen as error should be set to false
-			}
-			if(packedErrorScene != null) {
-				var instance = packedErrorScene.Instantiate();
-				GetTree().CurrentScene.AddChild(instance);
-			}
+			handleError(currError);
 		}
 	}
 
@@ -119,7 +122,7 @@ public partial class LevelUi : Node2D
 			errorNoticeIcon.QueueFree();
 			errorNoticeIcon = null;
 		}
-		error = false;
-		setCurrError(-1);
+		
+		removeError();
 	}
 }
