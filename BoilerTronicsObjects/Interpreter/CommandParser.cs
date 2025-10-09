a89fe14ebe2258df62b726c64ceb6d33f9f97ddb
Zach <zakirkeby@gmail.com>
2025-10-09 18:34:45 -0400

    TODO - multi params; mapping cont

---
+++ BoilerTronicsObjects/Interpreter/CommandParser.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using Godot;
+
+namespace CommandParser
+{
+	// Delegate is a silly c# thing that lets me treat stuff as objects
+	public delegate void CommandHandler(Match match);
+	public class CommandParser
+	{
+		private readonly List<(Regex Pattern, CommandHandler Handler)> _rules = new();
+		public void Register(string pattern, CommandHandler handler)
+		{
+			_rules.Add((new Regex(pattern, RegexOptions.IgnoreCase | RegexOptions.Compiled), handler));
+		}
+
+		public bool Process(string input)
+		{
+			foreach (var (pattern, handler) in _rules)
+			{
+				var match = pattern.Match(input);
+				if (match.Success)
+				{
+					handler(match);
+					GD.Print("Twas a match");
+					return false;
+				}
+				else
+				{
+                    //cases for error
+                    GD.Print("Not a match");
+					return false;
+				}
+			}
+			GD.Print($"Unknown command: {input}");
+			return false;
+		}
+	}
+}
