e54b57a276aa4f04d7b18bbbf2222faeed2a7224
dixon140 <dixon140@purdue.edu>
2025-10-14 14:32:39 -0400

    Add window for multiple saves

--- Scenes/LevelSelect.cs
+++ Scenes/LevelSelect.cs
@@ -4,9 +4,12 @@ using System;
 public partial class LevelSelect : Node2D
 {
 	private void _on_play_pressed() {
-		GetTree().ChangeSceneToFile("res://Scenes/level_ui.tscn");
+		GetNode<Window>("Window").Visible = true;
 	}
 	private void _on_back_pressed() {
 		GetTree().ChangeSceneToFile("res://Scenes/main_menu.tscn");
+	}
+	private void _on_window_close_requested() {
+		GetNode<Window>("Window").Visible = false;
 	}
 }
