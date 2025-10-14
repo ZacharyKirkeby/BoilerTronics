6bc053429068e5cee5c0f31efb6d7558d36bc432
ethensnchen <143560350+ethensnchen@users.noreply.github.com>
2025-10-14 14:55:43 -0400

    Story 11 progress
    this is the story 13 branch kek but anyways:
    - created a GlobalManager function that will allow for all draggable functionality to be toggled on/off as needed
    - the global manager no longer needs to be redefined in every function in LevelUi.cs, instead made a static field that will hold the global manager (i.e. just call 'manager')

--- BoilerTronicsObjects/Layers/Layer.cs
+++ BoilerTronicsObjects/Layers/Layer.cs
@@ -9,6 +9,9 @@ namespace BoilerTronicsObjects.Layers
 {
 	public partial class Layer : Godot.TileMapLayer
 	{
+		// if false, then should block all drag attempts
+		public static bool allowDrag = true;
+		
 		// default layer dimensions, if left unspecified
 		static int startX = 10;
 		static int startY = 10;
@@ -193,7 +196,9 @@ namespace BoilerTronicsObjects.Layers
 				}
 			} else {
 				// Left mouse click on a spot where an object exitsts
-				if (buttonEvent.ButtonIndex == MouseButton.Left && buttonEvent.IsPressed()) {
+				// Handles creating a new draggable object when clicking on a tile
+				if (buttonEvent.ButtonIndex == MouseButton.Left && buttonEvent.IsPressed()
+					&& allowDrag) {
 
 					// we don't went to do anything if we can;t find anything there
 					if (objAtPos == null) {
