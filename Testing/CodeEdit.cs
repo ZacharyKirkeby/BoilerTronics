bf57fb15a686195bcdbc24f08a6ac704190d4bba
ethensnchen <143560350+ethensnchen@users.noreply.github.com>
2025-10-15 00:09:50 -0400

    terminal highlighting selected object scaffolding
    - added scaffolding for terminal functionality to highlight/detect which object it corresponds to
      - also acquires the object's corresponding layer for rendering purposes
      - TODO: this process is INSANELY inefficient (i.e. just iterates through all layers and their corresponding object lists), look to update PlaceableObjects to optionally hold their parent layer! Will also need to update all code that create PlaceableObjects (i.e. call ObjectFactory) and etc

--- Testing/CodeEdit.cs
+++ Testing/CodeEdit.cs
@@ -1,11 +1,20 @@
 using Godot;
 using System;
+using System.Collections;				//	ArrayList
+using BoilerTronicsObjects.Placeable;	//	PlaceableObject
+using BoilerTronicsObjects.Layers;		//	all core layer functionality and etc
+using BoilerTronicsObjects.Interfaces;	//	Scriptable interface
 
 // whole file is arguably a test file
 public partial class CodeEdit : Godot.CodeEdit
 {
 	private int lastHighlightedLine = -1;
 	public int currentLine = 0;
+	
+	
+	// important vars for highlighting objects!
+	private PlaceableObject correspondingObject;
+	private bool highlightingObject = false;
 
 	public override void _Ready()
 	{
@@ -20,6 +29,71 @@ public partial class CodeEdit : Godot.CodeEdit
 	private void OnTextChanged()
 	{
 		GD.Print($"[{Name}] content changed:\n{Text}");
+	}
+	
+	// custom function, called by Terminals.cs
+	// intention is to use this function to check if 'correspondingObject' exists
+	// if so, try and "highlight" the object
+	// TODO: current Terminals.cs implementation doesn't call this properly on initial level creation
+	public void TerminalSelected() {
+		GD.Print("terminal selected");
+		
+		if (correspondingObject != null && (correspondingObject is Scriptable)) {
+			// highlight corresponding object
+			
+			// determine what layer this object is from
+			// TODO: we really should update the PlaceableObject objects to actually hold this data
+			BoilerTronicsGlobalManager manager = BoilerTronicsGlobalManager.GlobalManager;
+			
+			// try and find and highlight the specified object
+			bool res = HighlightObjectIfValid(correspondingObject, manager.layerClaw);
+			if (!res) { HighlightObjectIfValid(correspondingObject, manager.layerFactory); }
+			if (!res) { HighlightObjectIfValid(correspondingObject, manager.layerFloor); }
+			if (!res) { HighlightObjectIfValid(correspondingObject, manager.layerMovement); }
+			if (!res) { HighlightObjectIfValid(correspondingObject, manager.layerRail); }
+			
+		}
+	}
+	
+	// function to stop highlighting and etc
+	public void StopHighlighting() {
+		if (correspondingObject != null && highlightingObject) {
+			GD.Print("TODO: stop highlighting object");
+		}
+	}
+	
+	// terrible little helper function
+	// returns if the specified layer has the specified object
+	// refer to TerminalSelected; this should be deprecated ASAP when good datastructures are adopted and etc
+	private bool ObjectInLayer(PlaceableObject target, Layer layer) {
+		ArrayList work = layer.exportObjectList();
+		
+		// so very efficient (/s)
+		// TODO: update PlaceableObject datastruct to hold layer info, edit all instances where a PlaceableObject is constructed accordingly
+		foreach (PlaceableObject obj in work) {
+			if (obj == target) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	// yet another terrible little helper function
+	// if the object is in the specified layer, then highlight the specified object
+	private bool HighlightObjectIfValid(PlaceableObject target, Layer layer) {
+		// if object is not in layer, return
+		if (!ObjectInLayer(target, layer)) {return false;}
+		
+		// else: try and highlight the object!
+		// (TODO)
+		GD.Print("found target:", target);
+		return true;
+	}
+	
+	// sets internal object to point to input
+	// mainly just used for the "highlight terminal's corresponding object" functionality
+	public void SetCorrespondingObject(PlaceableObject input) {
+		correspondingObject = input;
 	}
 
 	public string GetCode()
