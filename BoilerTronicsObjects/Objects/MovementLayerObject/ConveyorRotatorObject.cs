15be737634065c742fa8bc2b37561db5cfe10898
keenan <keenankoehler@gmail.com>
2025-10-14 22:32:59 -0400

    get and set methods for scripts

--- BoilerTronicsObjects/Objects/MovementLayerObject/ConveyorRotatorObject.cs
+++ BoilerTronicsObjects/Objects/MovementLayerObject/ConveyorRotatorObject.cs
@@ -10,23 +10,36 @@ namespace BoilerTronicsObjects.Objects.MovementLayerObjects {
 	public class ConveyorRotatorObject : MovementLayerObjects, Scriptable, Runnable {
 
 		static Vector2I objectAtlasPos = new Vector2I(0, 2);
+		CodeEdit E;
 		// "atlasPos" corresponds to the location on a given sprite sheet that a specific object
 		// (i.e. "claw", "factory", "floor tile", "leftrail") will correspond to.
 		
 		public ConveyorRotatorObject(int OGX, int OGY, int altTitle = 0) 
 		: base(OGX, OGY, objectAtlasPos, altTitle) {}
 
-		public void CreateTerminal() {
-			// TODO: Implement
+		// Methods to deal with terminals
+		public CodeEdit GetTerminal() {
+			return E;
 		}
 
-		public CodeEdit GetTerminal() {
-			// TODO: Implement
-			return null;
+		public void CreateTerminal() {
+			BoilerTronicsGlobalManager manager = BoilerTronicsGlobalManager.GlobalManager;
+			E = manager.terminalContainer.AddEditor();
+			E.Name = "Claw";
 		}
 
 		public void DestroyTerminal() {
-			// TODO: Implement
+			BoilerTronicsGlobalManager manager = BoilerTronicsGlobalManager.GlobalManager;
+			manager.terminalContainer.RemoveEditor(E);
+			E = null;
+		}
+
+		public void SetScript(string script) {
+			E.Text = script;
+		}
+
+		public string GetScript() {
+			return E.Text;
 		}
 
 		public void Step() {
