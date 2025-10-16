b6f5868888c2d396f83391ed00e25dfff681c1ff
ethensnchen <143560350+ethensnchen@users.noreply.github.com>
2025-10-15 20:00:17 -0400

    terminal update
    - will not allow text when solution is in play
      - additionally, when attempting to provide input while solution is in play, highlighting system also does not re-trigger

--- Testing/Terminals.cs
+++ Testing/Terminals.cs
@@ -74,7 +74,12 @@ public partial class Terminals : TabContainer
 	{
 		
 		if (@event is InputEventKey keyEvent && keyEvent.Pressed)
-		{
+		{			
+			BoilerTronicsGlobalManager manager = BoilerTronicsGlobalManager.GlobalManager;
+			if (manager.currLevel.StepCount != 0) {
+				
+				return;
+			} 
 			// TODO: inefficient call if this runs every time the terminal at all updates!
 			UpdateSelectedTerminal();
 		
@@ -146,5 +151,13 @@ public partial class Terminals : TabContainer
 	public List<CodeEdit> GetAllEditors()
 	{
 		return editors;
+	}
+	
+	// set all sub editors editable or not
+	public void SetEditorsEditable(bool val) {
+		foreach (CodeEdit panel in editors) {
+			GD.Print("Terminals: set ", panel, " editable to ", val);
+			panel.SetEditable(val);
+		}
 	}
 }
