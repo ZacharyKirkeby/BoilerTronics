22f4af0e1f3073ed644ca37b6406d33ddddaab9c
keenan <keenankoehler@gmail.com>
2025-10-25 14:12:17 -0400

    simplified error system, needed to rework to seperate the error system from stepping for running

--- BoilerTronicsObjects/GameState/BoilerTronicsLevel.cs
+++ BoilerTronicsObjects/GameState/BoilerTronicsLevel.cs
@@ -25,6 +25,7 @@ public partial class BoilerTronicsLevel : Node2D
 	public ArrayList runnableList = new ArrayList(); // List of runnable Objects
 	public ArrayList movingList = new ArrayList(); // List of objects that are currently moving
 	public Parser P;
+	public ErrorHandler E;
 	
 	// store all four corners of the placement grid
 	private Vector2 c1;
@@ -271,29 +272,22 @@ public partial class BoilerTronicsLevel : Node2D
 	}
 
 	public void MovingCollisionReport(MovingObject mObj) {
-		var ui = GetTree().Root.GetNode<LevelUi>("/root/Node2D");
+		if (mObj == null) return; // We can't report a moving object
+		if (!(mObj.obj is PlaceableObject pObj)) return; // We can't report a moving object
 
-		HaultObjects();
+		HaultObjects(); // Stop all objects
 
-		if (mObj == null) return;
+		// Right now we only have collison for claws
+		if (pObj is Scriptable sObj) {
+			Layer parentLayer = pObj.GetParentLayer();
+			Vector2I gridPos = pObj.GetCurrPos();
 
-		// var clawObj = mObj.obj as ClawObject;
-		String editorName = "not found";
-		 if (mObj.obj is Scriptable scriptableObj) {
-			editorName = scriptableObj.GetTerminal()?.Name ?? "Unknown";
+			Vector2 localPos = parentLayer.MapToLocal(gridPos);
+			Vector2 globalPos = parentLayer.ToGlobal(localPos);
+
+			Vector2 offsetPos = globalPos + new Vector2(16, -16);
+
+			E.handleError(ErrorHandler.ErrorType.ClawCollision, sObj.GetTerminal(), offsetPos);
 		}
-		ui.setError(2, editorName);
-		GD.Print("COLLISION");	
-		Layer parentLayer = mObj.obj.GetParentLayer();
-		Vector2I gridPos = mObj.obj.GetCurrPos();
-
-		Vector2 localPos = parentLayer.MapToLocal(gridPos);
-		Vector2 globalPos = parentLayer.ToGlobal(localPos);
-
-		Vector2 offsetPos = globalPos + new Vector2(16, -16);
-		ui.setErrorCoords((Vector2I)offsetPos);
-
-		ui.handleError(ui.errorID, editorName);
-		return;
 	}
 }
