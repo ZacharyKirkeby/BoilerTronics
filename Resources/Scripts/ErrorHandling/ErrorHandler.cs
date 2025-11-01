77c113a049f8f32a17d86045372f3c37e1cc8607
Zach <zakirkeby@gmail.com>
2025-11-01 17:47:35 -0400

    highlighting is back bitches

--- Resources/Scripts/ErrorHandling/ErrorHandler.cs
+++ Resources/Scripts/ErrorHandling/ErrorHandler.cs
@@ -76,7 +76,7 @@ public partial class ErrorHandler : Node2D {
 				errorLabel.Position = new Vector2(0, editor.Size.Y - 20);
 
 				// Highlight error line
-				editor.HighlightLine(lineNumber, new Color(1, 0, 0, 0.25f));
+				editor.HighlightLine(lineNumber, new Color(1, 0, 0, 0.25f), true);
 
 				//TODO - delete
 				GD.Print($"[ParserError] {editorName}: Line {lineNumber} -> {message}");
