f19814a4857032a6da139fbdded52e271d1892be
Abhi <doshi47@purdue.edu>
2025-11-02 23:47:42 -0500

    quick fix for error highlighting

--- Testing/CodeEdit.cs
+++ Testing/CodeEdit.cs
@@ -21,7 +21,7 @@ public partial class CodeEdit : Godot.CodeEdit
 	private Layer highlightedLayer;
 	private bool highlightingObject = false;
 
-    public override void _Ready()
+	public override void _Ready()
 	{
 		AddToGroup("CodeTerminals");
 		HighlightCurrentLine = true;
