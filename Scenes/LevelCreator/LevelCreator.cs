64a1d94ada0af16a0da1315a33c07cf17fba0061
keenan <keenankoehler@gmail.com>
2025-11-03 22:36:22 -0500

    restabilized the branch lol

--- Scenes/LevelCreator/LevelCreator.cs
+++ Scenes/LevelCreator/LevelCreator.cs
@@ -64,9 +64,9 @@ public partial class LevelCreator : Node2D
 		AddChild(manager.currLevel.E); // Add as child so that we can access elements in the level
 
 		parser = GetNode<Parser>("/root/Node2D/MainVBox/TerminalLevelSplit/Parser");
-		parser.Connect(Parser.SignalName.ErrorRaised, new Callable(manager.currLevel.E, nameof(manager.currLevel.E.OnParserErrorRaised)));
+		// parser.Connect(Parser.SignalName.ErrorRaised, new Callable(manager.currLevel.E, nameof(manager.currLevel.E.OnParserErrorRaised)));
 
-		manager.currLevel.P = parser;
+		// manager.currLevel.P = parser;
 
 		var button = GetNode<Button>("MainVBox/PanelContainer/HBoxContainer/CategoryPicker/PlaceType1");
 		button.GrabFocus();
