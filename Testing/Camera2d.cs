40390974400b028819cfdf2c0eb89d2ac5b8d8b2
ethensnchen <143560350+ethensnchen@users.noreply.github.com>
2025-11-03 20:12:51 -0500

    zoom functionality update
    - zooming now properly zooms in at the user's mouse position
      - this was a bit annoying to fix, but at least it works now.
      - zoom functionality is also now multiplicative and not additive; makes for a more consistent zooming experience.

--- Testing/Camera2d.cs
+++ Testing/Camera2d.cs
@@ -4,9 +4,19 @@ using System;
 namespace BoilerTronicsObjects.GameCamera {
 	public partial class Camera2d : Camera2D
 	{
+		BoilerTronicsGlobalManager manager = BoilerTronicsGlobalManager.GlobalManager;
+		
+		// tracks right mouse button held
 		private bool rmbHeld = false;
-		private float zoomDiff = (float) 0.05;
-		private Vector2 zoomFloat = new Vector2((float) 0.05, (float) 0.05);
+		
+		private const float zoomFactor = 0.1f;
+		 
+		// additive zoom
+		private Vector2 zoomFloat = new Vector2(zoomFactor,  zoomFactor);
+		
+		// multiplicative zoom
+		private Vector2 zoomMultP = new Vector2(1f + zoomFactor, 1f + zoomFactor);
+		private Vector2 zoomMultN = new Vector2(1f - zoomFactor, 1f - zoomFactor);
 		
 		private Vector2 maxZoom = new Vector2((float) 3.0, (float) 3.0);
 		private Vector2 minZoom = new Vector2((float) 0.5, (float) 0.5);
@@ -14,67 +24,63 @@ namespace BoilerTronicsObjects.GameCamera {
 		private Vector2 errorPosition = new Vector2(150,500);
 		private Sprite2D errorSprite;
 
-		// general zoom function to zoom specifically at the mouse; TODO
+
+		// general zoom function to zoom specifically at the mouse
 		public void zoomToTarget(Vector2 zoomTarget) {
-			
-			// note: following code is from the Godot forum -- will need tweaking!
-			/*
-			Vector2 halfSize = GetViewport().GetVisibleRect().Size / (float) 2.0;
-			Vector2 cameraPosition = this.Position;
-			Vector2 point = GetLocalMousePosition();
-			Vector2 newCameraPosition;
-			Vector2 z0 = this.Zoom;
-			Vector2 z1 = zoomTarget;
-
-			newCameraPosition = cameraPosition + (-halfSize + point) * (z0 - z1);
-			this.SetZoom(zoomTarget);
-			this.Position = newCameraPosition;
-			*/
-			
-			// Vector2 p0 = GetLocalMousePosition();
+			// move zoom target to center of screen
+			Vector2 p0 = GetLocalMousePosition();
 			this.Zoom = zoomTarget;
-			// Vector2 p1 = GetLocalMousePosition();
-			// Vector2 diff = p0 - p1;
+			Vector2 p1 = GetLocalMousePosition();
+			Vector2 diff = p1 - p0;
 			// GD.Print("diff: ",  diff);
-			// this.Position += diff;
-
+			this.Position -= diff;
 		}
 		
-		public override void _Input(InputEvent @event)
-			{
-				// Note: these inputs will still "pass through" and Layer.cs could theoretically receive this
-				// Be advised!
+		// handle mouse inputs (RMB, scroll wheel)
+		public override void _Input(InputEvent @event) {
+			// Note: these inputs will still "pass through" and Layer.cs could theoretically receive this (?)
+			// Be advised!
 
-				if (@event is InputEventMouseButton buttonEvent) {
-					
-					if (buttonEvent.ButtonIndex == MouseButton.Right) {
-						if (buttonEvent.Pressed) {
-							rmbHeld = true;
-						} else {
-							rmbHeld = false;
-						}
-					} else if (buttonEvent.ButtonIndex == MouseButton.WheelUp) {
+			// all mouse button events
+			if (@event is InputEventMouseButton buttonEvent) {
+				
+				// catch all button events related to RMB
+				if (buttonEvent.ButtonIndex == MouseButton.Right) {
+					if (buttonEvent.Pressed) {
+						rmbHeld = true;
+					} else {
+						rmbHeld = false;
+					}
+				}
+				
+				// "@event.IsPressed()" is important to avoid double-catching inputs!
+				// only works if we're looking to see if the event was pressed, however
+				if (@event.IsPressed()) {
+					if (buttonEvent.ButtonIndex == MouseButton.WheelUp) {
 						// zoom in
-						
-						Vector2 zoomIn = this.GetZoom() + zoomFloat;
+						Vector2 zoomIn = this.GetZoom() * zoomMultP;//this.GetZoom() + zoomFloat;
 						if (zoomIn < maxZoom) {
+							// GD.Print("zoom in");
 							zoomToTarget(zoomIn);
 						}
 					} else if (buttonEvent.ButtonIndex == MouseButton.WheelDown) {
 						// zoom out
-						
-						Vector2 zoomOut = this.GetZoom() - zoomFloat;
+						Vector2 zoomOut = this.GetZoom() * zoomMultN;//this.GetZoom() - zoomFloat;
 						if (zoomOut > minZoom) {
+							// GD.Print("zoom out");
 							zoomToTarget(zoomOut);
 						}
 					}
 				}
-				
-				if (@event is InputEventMouseMotion eventMouseMotion && rmbHeld) {
-					// need to account for zoom level!
-					this.Position += eventMouseMotion.GetScreenRelative() * -1 / this.GetZoom().X;//new Vector2(5, 5);
-				}
+				// end
 			}
+			
+			
+			if (@event is InputEventMouseMotion eventMouseMotion && rmbHeld) {
+				// need to account for zoom level!
+				this.Position += eventMouseMotion.GetScreenRelative() * -1 / this.GetZoom().X;//new Vector2(5, 5);
+			}
+		}
 			
 		public void SpawnErrorSprite(Vector2 errorPosition) {
 			//if coords are negative/invalid do not spawn
