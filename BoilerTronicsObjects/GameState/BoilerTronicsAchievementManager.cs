e33cf3f770c910a47cf79d644861705b60f62495
Abhi <doshi47@purdue.edu>
2025-11-15 12:26:56 -0500

    added achievement auto popup and easter egg setup

--- BoilerTronicsObjects/GameState/BoilerTronicsAchievementManager.cs
+++ BoilerTronicsObjects/GameState/BoilerTronicsAchievementManager.cs
@@ -7,6 +7,7 @@ public partial class BoilerTronicsAchievementManager : Node
 		public static BoilerTronicsAchievementManager AchievementManager { get; private set; }
 		
 		private Dictionary<string, bool> achievements = new();
+		private Dictionary<string, bool> eastereggs = new();
 		
 		public override void _Ready() {
 			if (AchievementManager != null) {
@@ -24,6 +25,11 @@ public partial class BoilerTronicsAchievementManager : Node
 			achievements["Achievement2"] = false;
 			achievements["Achievement3"] = false;
 			achievements["Achievement4"] = false;
+			
+			eastereggs["EasterEgg1"] = false;
+			eastereggs["EasterEgg2"] = false;
+			eastereggs["EasterEgg3"] = false;
+			eastereggs["EasterEgg4"] = false;
 		}
 
 		public void UnlockAchievement(string name) {
@@ -40,11 +46,33 @@ public partial class BoilerTronicsAchievementManager : Node
 			achievements[name] = true;
 		}
 
-		public bool IsUnlocked(string name) {
+		public bool AchievementIsUnlocked(string name) {
 			return achievements.ContainsKey(name) && achievements[name];
 		}
 		
 		public Dictionary<string, bool> GetAllAchievements() {
 			return achievements;
+		}
+		
+		public void UnlockEasterEgg(string name) {
+			if (!eastereggs.ContainsKey(name)) {
+				GD.PrintErr("Easter Egg did not unlock");
+				return;
+			}
+
+			if(eastereggs[name]) {
+				GD.Print("Easter Egg already unlocked");
+				return;
+			}
+
+			eastereggs[name] = true;
+		}
+
+		public bool EasterEggIsUnlocked(string name) {
+			return eastereggs.ContainsKey(name) && eastereggs[name];
+		}
+		
+		public Dictionary<string, bool> GetAllEasterEggs() {
+			return eastereggs;
 		}
 }
---
+++ Scenes/MainMenu/EasterEgg.cs
@@ -0,0 +1,72 @@
+using Godot;
+using System;
+
+public partial class EasterEgg : Window
+{
+	static private Label EasterEgg1Desc;
+	static private Label EasterEgg2Desc;
+	static private Label EasterEgg3Desc;
+	
+	static private TextureRect EasterEgg1Image;
+	static private TextureRect EasterEgg2Image;
+	static private TextureRect EasterEgg3Image;
+	
+	public override void _Ready() {
+		EasterEgg1Desc = GetNode<Label>("%EasterEgg1 Description");
+		EasterEgg2Desc = GetNode<Label>("%EasterEgg2 Description");
+		EasterEgg3Desc = GetNode<Label>("%EasterEgg3 Description");
+		
+		EasterEgg1Image = GetNode<TextureRect>("%EasterEgg1 Image");
+		EasterEgg2Image = GetNode<TextureRect>("%EasterEgg2 Image");
+		EasterEgg3Image = GetNode<TextureRect>("%EasterEgg3 Image");
+		
+		UpdateUI();
+	}
+	
+	private void UpdateUI() {
+		var achievementManager = BoilerTronicsAchievementManager.AchievementManager;
+		var allEasterEggs = achievementManager.GetAllEasterEggs();
+		
+		foreach(var pair in allEasterEggs) {
+			string name = pair.Key;
+			bool unlocked = pair.Value;
+			UpdateEasterEgg(name, unlocked);
+		}
+	}
+	
+	private void UpdateEasterEgg(string name, bool unlocked) {
+		Label desc = null;
+		TextureRect icon = null;
+		switch (name)
+		{
+			case "EasterEgg1":
+				desc = EasterEgg1Desc;
+				icon = EasterEgg1Image;
+				if(unlocked) {
+					desc.Text = "Found: Solved a solution with 100+ time steps over the cutoff.";
+				}
+				break;
+			case "EasterEgg2":
+				desc = EasterEgg2Desc;
+				icon = EasterEgg2Image;
+				if(unlocked) {
+					desc.Text = "Found: Solved a solution $5,000+ over the cutoff.";
+				}
+				break;
+			case "EasterEgg3":
+				desc = EasterEgg3Desc;
+				icon = EasterEgg3Image;
+				if(unlocked) {
+					desc.Text = "Found: You found the secret level!";
+				}
+				break;
+			default:
+				GD.PrintErr($"Unknown easter egg: {name}");
+				return;
+		}
+		if(unlocked) {
+			desc.Text = "Found: " + desc.Text;
+			icon.Texture = GD.Load<Texture2D>("res://Resources/Icons/star.png");
+		}
+	}
+}
