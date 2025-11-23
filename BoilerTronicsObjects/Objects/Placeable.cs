1e6f35b064ab379781ad6b16e44483d27eac646c
keenan <keenankoehler@gmail.com>
2025-11-23 14:53:56 -0500

    heated test material done, still need machines that use the heated material to test it

--- BoilerTronicsObjects/Objects/Placeable.cs
+++ BoilerTronicsObjects/Objects/Placeable.cs
@@ -157,6 +157,7 @@ namespace BoilerTronicsObjects.Placeable
 			};
 		}
 		
+		/*
 		// Used for ArrayList.Contains and etc
 		// Not 100% conclusive! Potential edge case is if two objects, identical on the surface level
 		// and sharing the same coordinates, but on different Layers, this will
@@ -173,5 +174,6 @@ namespace BoilerTronicsObjects.Placeable
 				this.GetAtlasPos() == cObj.GetAtlasPos()
 			);
 		}
+		*/
 	}
 }
