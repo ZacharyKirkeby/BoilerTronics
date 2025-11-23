4e094e65524e96eadb3213158e36c17fce5e0001
keenan <keenankoehler@gmail.com>
2025-11-23 13:56:54 -0500

    pipes changing sprites based on connection

--- BoilerTronicsObjects/Objects/FactoryLayerObject/PipeObject.cs
+++ BoilerTronicsObjects/Objects/FactoryLayerObject/PipeObject.cs
@@ -13,29 +13,61 @@ namespace BoilerTronicsObjects.Objects.FactoryLayerObjects {
 		public override int GetCost() { return 100; }
 		public new static int GetCostStatic() { return 100; }
 
-		// We'll need a better way to detect this
-		// TODO: make some enum or something to store the direction of the pipe
-		// (each bit is a dir, 0 = not connected, 1 = connected)
-		// public const int Left = 0;
-		// public const int Right = 1;
-
-		int direction;
-		
 		static int layerSourceId = 13;
 
-		// reminder that the sourceID corresponds to the sprite sheet for a given layer
-		// and every layer will have their own sprite sheet. Consequently, layer-specific
-		// objects will have identical sourceIds.
+		// bit 0 = up conn
+		// bit 1 = right conn
+		// bit 2 = down conn
+		// bit 3 = left conn
+		public enum Direction {
+			NOCONN = 	0b0000,
+			UCONN =		0b0001,
+			RCONN =		0b0010,
+			URCONN = 	0b0011,
+			DCONN =		0b0100,
+			UDCONN = 	0b0101,
+			RDCONN = 	0b0110,
+			URDCONN = 	0b0111,
+			LCONN =		0b1000,
+			ULCONN = 	0b1001,
+			RLCONN = 	0b1010,
+			URLCONN = 	0b1011,
+			DLCONN = 	0b1100,
+			UDLCONN = 	0b1101,
+			RDLCONN = 	0b1110,
+			ALLCONN = 	0b1111
+		};
+
+		Direction direction;
+
+		private static Vector2I []sprites = [
+			new Vector2I(0,0), // Default
+			new Vector2I(0,0), // Up
+			new Vector2I(1,0), // Right
+			new Vector2I(1,2), // Up, Right
+			new Vector2I(0,0), // Down
+			new Vector2I(0,0), // Up, Down
+			new Vector2I(0,2), // Right, Down
+			new Vector2I(1,1), // Up, Right, Down
+			new Vector2I(1,0), // Left
+			new Vector2I(2,2), // Up, Left
+			new Vector2I(1,0), // Right, Left
+			new Vector2I(3,0), // Up, Right, Left
+			new Vector2I(3,2), // Down, Left
+			new Vector2I(0,1), // Up, Donw. Left
+			new Vector2I(2,0), // Right, Down, Left
+			new Vector2I(2,1)  // Up, Right, Down, Left
+		];
+
+		// We'll need to store these in a list, we can more than likely have these at the index of the corresponding enum
+		static Vector2I LeftObjectAtlasPos = new Vector2I(0, 0);
+		static Vector2I RightObjectAtlasPos = new Vector2I(0, 1);
 
 		private PipeGroup group;
 
 		// For loading purposes, have a specific string that will override its parent's group contents
 		private string internalText = null;
 
-
-		// We'll need to store these in a list, we can more than likely have these at the index of the corresponding enum
-		static Vector2I LeftObjectAtlasPos = new Vector2I(0, 0);
-		static Vector2I RightObjectAtlasPos = new Vector2I(0, 1);
 		
 		// Gets the group that this object belongs to
 		public GroupedObject getGroup() {
@@ -61,24 +93,39 @@ namespace BoilerTronicsObjects.Objects.FactoryLayerObjects {
 			return new PipeGroup(this.GetCurrPos().X, this.GetCurrPos().Y) as GroupedObject;
 		}
 
-		private void UpdateSprite() {
+		public void UpdateSprite() {
 			BoilerTronicsGlobalManager manager = BoilerTronicsGlobalManager.GlobalManager;
 
-			manager.currLevel.mLayer.SetCell(this.GetCurrPos(), this.GetSourceID(), this.GetAtlasPos()); // Set the new sprite
+			int u = 0;
+			int d = 0;
+			int l = 0;
+			int r = 0;
+
+			Vector2I uv = new Vector2I(1, -1);
+			Vector2I dv = new Vector2I(-1, 1);
+			Vector2I lv = new Vector2I(-1, 0);
+			Vector2I rv = new Vector2I(1, 0);
+
+			PlaceableObject obju = manager.currLevel.fLayer.FindObject(this.GetCurrPos() + uv);
+			if (obju != null && obju is PipeObject) u = 1;
+
+			PlaceableObject objd = manager.currLevel.fLayer.FindObject(this.GetCurrPos() + dv);
+			if (objd != null && objd is PipeObject) d = 1;
+			
+			PlaceableObject objr = manager.currLevel.fLayer.FindObject(this.GetCurrPos() + rv);
+			if (objr != null && objr is PipeObject) r = 1;
+
+			PlaceableObject objl = manager.currLevel.fLayer.FindObject(this.GetCurrPos() + lv);
+			if (objl != null && objl is PipeObject) l = 1;
+
+			direction = (Direction) ((u) | (r << 1) | (d << 2) | (l << 3));
+
+			SetAtlasPos(sprites[(int) direction]);
+
+			manager.currLevel.fLayer.SetCell(this.GetCurrPos(), this.GetSourceID(), this.GetAtlasPos()); // Set the new sprite
 		}
 
 		public PipeObject(int OGX, int OGY, int altTitle = 0) : base(OGX, OGY, layerSourceId, new Vector2I(0,0), altTitle) {
-		}
-
-		// TODO: this should update based on the connections instead of a passed in val
-		public void ChangeDir(int newDir) {
-			direction = newDir;
-
-			UpdateSprite();
-		}
-
-		public int GetDir() {
-			return direction;
 		}
 
 		public List<PipeObject> GetConnections() {
@@ -91,16 +138,16 @@ namespace BoilerTronicsObjects.Objects.FactoryLayerObjects {
 
 			List<PipeObject> retList = new List<PipeObject>();
 			
-			PlaceableObject obj1 = manager.currLevel.mLayer.FindObject(this.GetCurrPos() + v1);
+			PlaceableObject obj1 = manager.currLevel.fLayer.FindObject(this.GetCurrPos() + v1);
 			if (obj1 != null && obj1 is PipeObject pObj1) retList.Add(pObj1);
 
-			PlaceableObject obj2 = manager.currLevel.mLayer.FindObject(this.GetCurrPos() + v2);
+			PlaceableObject obj2 = manager.currLevel.fLayer.FindObject(this.GetCurrPos() + v2);
 			if (obj2 != null && obj2 is PipeObject pObj2) retList.Add(pObj2);
 
-			PlaceableObject obj3 = manager.currLevel.mLayer.FindObject(this.GetCurrPos() + v3);
+			PlaceableObject obj3 = manager.currLevel.fLayer.FindObject(this.GetCurrPos() + v3);
 			if (obj3 != null && obj3 is PipeObject pObj3) retList.Add(pObj3);
 
-			PlaceableObject obj4 = manager.currLevel.mLayer.FindObject(this.GetCurrPos() + v2);
+			PlaceableObject obj4 = manager.currLevel.fLayer.FindObject(this.GetCurrPos() + v4);
 			if (obj4 != null && obj4 is PipeObject pObj4) retList.Add(pObj4);
 
 			return retList;
