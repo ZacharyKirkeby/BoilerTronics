acedb15cc2e2dd6a2cf2c6052cce6b365541ffac
keenan <keenankoehler@gmail.com>
2025-12-03 12:40:54 -0500

    error when reseting (modifying runlist during the reset step) now fixed

--- BoilerTronicsObjects/GameState/BoilerTronicsLevel.cs
+++ BoilerTronicsObjects/GameState/BoilerTronicsLevel.cs
@@ -548,9 +548,14 @@ public partial class BoilerTronicsLevel : Node2D
 			mObj.QueueFree();
 		}
 
-		foreach (Runnable rObj in runnableList)
-		{
-			rObj.Reset();
+		foreach (List<PlaceableObject> RL in runList) {
+			for (int i = RL.Count - 1; i >= 0; i--)
+    			{
+				PlaceableObject obj = RL[i];
+				if (!(obj is Runnable)) continue;
+				Runnable rObj = (Runnable)obj;
+				rObj.Reset();
+			}
 		}
 		
 		StepCount = 0;
--- BoilerTronicsObjects/Objects/FactoryLayerObject/Materials/SteelGearObject.cs
+++ BoilerTronicsObjects/Objects/FactoryLayerObject/Materials/SteelGearObject.cs
@@ -93,6 +93,7 @@ namespace BoilerTronicsObjects.Objects.FactoryLayerObjects {
 			heatValue = 0;
 			ResetPos();
 			holder.deleteHeld();
+			UnRegisterSteppable();
 		}
 
 		public void RegisterSteppable() {
