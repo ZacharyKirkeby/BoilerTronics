6041d38ddadb85ad0cccde7d4ae4007b95a1805f
Zach <zakirkeby@gmail.com>
2025-12-03 15:41:39 -0500

    data classes

---
+++ Scenes/AuthScripts/ScoreData.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+
+// what can i say i sometimes like objects
+public class ScoreData
+{
+	public string UserId { get; set; }
+	public string Username { get; set; }
+	public string LevelId { get; set; }
+	public int Score { get; set; }
+	public float TimeSeconds { get; set; }
+	public DateTime Timestamp { get; set; }
+	public Dictionary<string, object> Metadata { get; set; }
+}
