213ccff5a9e383f7cb2e17988c392ed98642b1a2
ethensnchen <143560350+ethensnchen@users.noreply.github.com>
2025-12-04 22:32:42 -0500

    full intended selection functionality
    - after just re-writing the logic i came to an actually good solution
      - Claw layer passes down input into the Rail layer
      - Factory layer passes down input into the Floor layer
    
    - i.e. claw, factory layer objects will always take priority

--- BoilerTronicsObjects/Layers/FloorLayer.cs
+++ BoilerTronicsObjects/Layers/FloorLayer.cs
@@ -24,23 +24,8 @@ namespace BoilerTronicsObjects.Layers
 
 		public override void _Input(InputEvent @event)
 		{	
-			// add a check to make sure that we are only trying to place floors
-			BoilerTronicsGlobalManager manager = BoilerTronicsGlobalManager.GlobalManager;
-			if (manager.currSlection != 2) {return;}
-			// GD.Print("FloorLayer: Working Input");
-			/*
-			// MouseInput(@event, 2, 0);
-			if (manager.objectToMove is FloorLayerObject) {
-				MouseInput(@event, 2);
-				// GD.Print("Floor");
-			}
-			else if (@event is InputEventMouseButton buttonEvent && (buttonEvent.ButtonIndex == MouseButton.Left || buttonEvent.ButtonIndex == MouseButton.Right) && buttonEvent.IsPressed()) {
-				// We always wnt to try to move
-				// MouseInput(@event, 2);
-				return;
-			}
+			// do nothing -- we should only ever receive pass-through events fom the FactoryLayer!
 			base._Input(@event);
-			*/
 		}
 		
 		
