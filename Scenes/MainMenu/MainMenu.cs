8d7b1940125d131fd2b4b18a2420ea5ab07eac58
ethensnchen <143560350+ethensnchen@users.noreply.github.com>
2025-12-04 20:21:51 -0500

    auth/firestore service accessibility tweak
    - now the FirebaseAuthManager and FirestoreService used by MainMenu.cs and Leaderboard.cs are only handled in MainMenu.cs

--- Scenes/MainMenu/MainMenu.cs
+++ Scenes/MainMenu/MainMenu.cs
@@ -4,11 +4,21 @@ using System;
 public partial class MainMenu : Node2D
 {
 	private int level = 11;
-	private FirebaseAuthManager _authManager;
-	private FirestoreService _firestoreService;
+	private static FirebaseAuthManager _authManager;
+	private static FirestoreService _firestoreService;
+	
+	public static MainMenu mainMainMenu;
+	
+	public static FirebaseAuthManager GetFirebaseAuthManager() {
+		return _authManager;
+	}
+	public static FirestoreService GetFirestoreService() {
+		return _firestoreService;
+	}
 	
 	public override void _Ready()
 	{
+		mainMainMenu = this;
 		InitializeServices();
 
 		// Set fullscreen toggle
@@ -23,14 +33,14 @@ public partial class MainMenu : Node2D
 		
 	}
 
-	private void InitializeServices()
+	public static void InitializeServices()
 	{
 		// Get or create auth manager
 		_authManager = FirebaseAuthManager.Instance;
 		if (_authManager == null)
 		{
 			_authManager = new FirebaseAuthManager();
-			AddChild(_authManager);
+			mainMainMenu.AddChild(_authManager);
 		}
 
 		// Get or create firestore service
@@ -38,7 +48,7 @@ public partial class MainMenu : Node2D
 		if (_firestoreService == null)
 		{
 			_firestoreService = new FirestoreService();
-			AddChild(_firestoreService);
+			mainMainMenu.AddChild(_firestoreService);
 		}
 	}
 
